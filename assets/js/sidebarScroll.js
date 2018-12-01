ca58cef7f25e1cab85cf623761a20291743561fa
Birk Ingebricson Wilberg <birk@ingebricson.no>
2018-12-01 14:54:37 +0100

    Fixed sticky sticking to bottom on phone for runar sin pc <3

--- assets/js/sidebarScroll.js
+++ assets/js/sidebarScroll.js
@@ -1,12 +1,14 @@
 var stickystart = document.getElementById('stickystart');
 var stickybox = document.getElementById('stickybox');
 
-window.addEventListener('scroll', () => {    
-    if (window.scrollY >= stickystart.offsetTop + stickystart.clientHeight - stickybox.clientHeight) {
-        stickybox.setAttribute('class', 'unsticked-bottom');
-    } else if (window.scrollY >= stickystart.offsetTop + 20) {
-        stickybox.setAttribute('class', 'sticked');
-    } else {
-        stickybox.removeAttribute('class');
+window.addEventListener('scroll', () => {
+    if (window.innerWidth > 850) {
+        if (window.scrollY >= stickystart.offsetTop + stickystart.clientHeight - stickybox.clientHeight) {
+            stickybox.setAttribute('class', 'unsticked-bottom');
+        } else if (window.scrollY >= stickystart.offsetTop + 20) {
+            stickybox.setAttribute('class', 'sticked');
+        } else {
+            stickybox.removeAttribute('class');
+        }
     }
 });
