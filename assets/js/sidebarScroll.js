4ee48b8d9f6742c68e70b8c578daee54e6a482ed
Brage Allum <brageallum@gmail.com>
2018-12-01 14:45:30 +0100

    Fixed sticky bar for runar sin pc <3

--- assets/js/sidebarScroll.js
+++ assets/js/sidebarScroll.js
@@ -1,8 +1,10 @@
 var stickystart = document.getElementById('stickystart');
 var stickybox = document.getElementById('stickybox');
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY >= stickystart.offsetTop + 20) {
+window.addEventListener('scroll', () => {    
+    if (window.scrollY >= stickystart.offsetTop + stickystart.clientHeight - stickybox.clientHeight) {
+        stickybox.setAttribute('class', 'unsticked-bottom');
+    } else if (window.scrollY >= stickystart.offsetTop + 20) {
         stickybox.setAttribute('class', 'sticked');
     } else {
         stickybox.removeAttribute('class');
